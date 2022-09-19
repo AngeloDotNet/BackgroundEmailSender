afed30c44755e91dab9805e3e304b8e4a0fd8b16
Angelo Pirola <angelo9871@gmail.com>
2022-09-19 11:13:51 +0200

    Refactoring Controllers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,29 +1,19 @@
-﻿using System.Threading.Tasks;
-using BackgroundEmailSenderSample.Models.InputModels;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+﻿namespace BackgroundEmailSenderSample.Controllers;
 
-namespace BackgroundEmailSenderSample.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-
-        [HttpPost]
-        public async Task<IActionResult> SendMail(ContactInputModel inputModel, [FromServices] IEmailSender emailSender) 
-        {
-            await emailSender.SendEmailAsync(inputModel.Email, "Request from our website", inputModel.ToHtmlMessage());
-            return RedirectToAction(nameof(ThankYou));
-        }
-        
-        public IActionResult ThankYou() {
-            return View();
-        }
-
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> SendMail(ContactInputModel inputModel, [FromServices] IEmailSender emailSender) 
+    {
+        await emailSender.SendEmailAsync(inputModel.Email, "Request from our website", inputModel.ToHtmlMessage());
+        return RedirectToAction(nameof(ThankYou));
+    }
+    
+    public IActionResult ThankYou() {
+        return View();
     }
 }

579b3c71469a6da8631b0415c5d747bd51d9d771
Angelo Pirola <angelo9871@gmail.com>
2026-01-17 00:25:29 +0100

    Refactor to use 'var' for builder and app declarations
    Replaced explicit type declarations with 'var' for the builder and app variables in Program.cs. This improves code conciseness and utilizes type inference.

--- Program.cs
+++ Program.cs
@@ -5,14 +5,14 @@ public class Program
     public static void Main(string[] args)
     {
         // Vari esempi per usare il nuovo builder: https://docs.microsoft.com/en-us/aspnet/core/migration/50-to-60-samples
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+        var builder = WebApplication.CreateBuilder(args);
 
         Startup startup = new(builder.Configuration);
 
         // Aggiungere i servizi per la dependency injection (metodo ConfigureServices)
         startup.ConfigureServices(builder.Services);
 
-        WebApplication app = builder.Build();
+        var app = builder.Build();
 
         // Usiamo i middleware (metodo Configure)
         startup.Configure(app);

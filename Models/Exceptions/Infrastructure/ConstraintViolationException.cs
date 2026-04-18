8680d22b6447fa93a015d4f8d57ab61c605e3c95
Angelo Pirola <angelo9871@gmail.com>
2026-04-19 00:03:07 +0200

    Add XML docs to ConstraintViolationException class
    Added detailed XML documentation to the ConstraintViolationException class, including summary, remarks, and parameter descriptions to clarify its usage and intent when handling database constraint violations.

--- Models/Exceptions/Infrastructure/ConstraintViolationException.cs
+++ Models/Exceptions/Infrastructure/ConstraintViolationException.cs
@@ -1,5 +1,11 @@
 namespace BackgroundEmailSenderSample.Models.Exceptions.Infrastructure;
 
-public class ConstraintViolationException(Exception innerException)
-    : Exception($"A violation occurred for a database constraint", innerException)
+/// <summary>
+/// Represents an exception that is thrown when a database constraint is violated.
+/// </summary>
+/// <remarks>This exception is typically thrown when an operation fails due to a violation of a database
+/// constraint, such as a unique key or foreign key constraint. Use this exception to distinguish constraint violations
+/// from other types of database errors.</remarks>
+/// <param name="innerException">The exception that caused the current exception. This value cannot be null.</param>
+public class ConstraintViolationException(Exception innerException) : Exception($"A violation occurred for a database constraint", innerException)
 { }
